920a8d6b754991feb0c8e227f04e71e3e17a4174
parkrye <62276545+parkrye@users.noreply.github.com>
2023-04-17 16:27:06 +0900

    Add files via upload

---
+++ KGA/00_CSharpProgramming/12.Event_Details/Basic/UI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Event.Basic
+{
+    internal class UI
+    {
+        public void CoinUI()
+        {
+            Console.WriteLine("현재 코인 수 늘어남");
+        }
+    }
+}
---
+++ KGA/00_CSharpProgramming/13.Additional.cs
@@ -0,0 +1,13 @@
+﻿namespace _13.Additional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Additional additional = new();
+            additional.Test();
+            Partial partial = new();
+            partial.Test();
+        }
+    }
+}
