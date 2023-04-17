a2809f74aeaafcccecabeeed45d72727ce762e45
parkrye <parkrye@gmail.com>
2023-04-17 18:10:04 +0900

    feat: Add GithubTest Code

---
+++ GithubTest/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTest
+{
+    internal class Player: IAttackable, IDamageTakable
+    {
+        string name;
+        public string NAME
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        int hp;
+        public int HP
+        {
+            get { return hp; }
+            set { hp = value; }
+        }
+        int ap;
+        public int AP
+        {
+            get { return ap; }
+            set { ap = value; }
+        }
+
+        public void Attack(IDamageTakable damageTakable)
+        {
+            Console.WriteLine($"{name} attack {damageTakable}");
+            damageTakable.TakeDamage(ap);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Console.WriteLine($"{name} take {damage} damage");
+        }
+    }
+}
---
+++ GithubTest/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GithubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new();
+            Monster monster = new();
+
+            player.NAME = "플레이어";
+            monster.NAME = "몬스터";
+
+            player.Attack(monster);
+        }
+    }
+}
