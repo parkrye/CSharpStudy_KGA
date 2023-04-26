a717085cb3665e515ed9348c0560a3de1ddf439a
parkrye <parkrye@gmail.com>
2023-04-26 19:14:25 +0900

    test: Project RPG
    추가한 마을 기능을 위한 테스트 코드 작성

--- 02_CSharpAlgorithm/PersonalProject/ProjectRPG/Program.cs
+++ 02_CSharpAlgorithm/PersonalProject/ProjectRPG/Program.cs
@@ -7,25 +7,11 @@
             Console.CursorVisible = false;
 
             Player player = new Player();
+            player.AddItem(new Item_HPPotion3());
+            player.AddItem(new Item_HPPotion3());
 
-            PC pc11 = new PC(new Class_Figther());
-            pc11.ITEMSLOT.AddItem(new Item_MysteriousRing());
-            player.EmployCharacter(pc11);
-            player.EmployedToParty(0);
-
-            PC pc21 = new PC(new Class_Clown());
-            PC pc22 = new PC(new Class_Clown());
-            PC pc23 = new PC(new Class_Clown());
-            PC pc24 = new PC(new Class_Clown());
-            Party enemyParty = new Party();
-            pc21.ITEMSLOT.AddItem(new Item_BronzeCoin());
-            enemyParty.AddPC(pc21);
-            enemyParty.AddPC(pc22);
-            enemyParty.AddPC(pc23);
-            enemyParty.AddPC(pc24);
-
-            Battle battle = new Battle(player, enemyParty);
-            battle.StartBattle();
+            Town town = new Town();
+            town.GetIn(player);
         }
     }
 }
