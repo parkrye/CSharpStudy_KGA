15c49d469a767ae9c2dc68336a6193e0083f0bd9
parkrye <parkrye@gmail.com>
2023-04-26 17:19:25 +0900

    feat: Project RPG
    PC 생성자 수정 및 추가

--- 02_CSharpAlgorithm/PersonalProject/ProjectRPG/Characters/PC.cs
+++ 02_CSharpAlgorithm/PersonalProject/ProjectRPG/Characters/PC.cs
@@ -6,10 +6,9 @@
     internal class PC : Character
     {
         /// <summary>
-        /// PC 생성자
+        /// 무직업 생성자
         /// </summary>
-        /// <param name="c">캐릭터의 직업</param>
-        public PC(Class c = null)
+        public PC()
         {
             Random random = new Random();
 
@@ -25,38 +24,76 @@
             MAX_PHYSICSAL = random.Next(5, 15);
             MAX_MENTAL = random.Next(5, 15);
             MAX_INITIATIVE = random.Next(5, 15);
-            if (c != null)
-            {
-                MAX_HP += c.HP;
-                MAX_SP += c.SP;
-                MAX_PHYSICSAL += c.PHYSICAL;
-                MAX_MENTAL += c.MENTAL;
-                MAX_INITIATIVE += c.INITIATIVE;
-            }
             StatusSetting(true);
-            for (int i = 0; i < 5; i++)
-                DIFFICULTY += status[0, i] / 10;
 
             // 스킬 슬롯은 일반적으로 최대 3
             SKILLSLOT = new SkillSlot(this, 3);
             SKILLSLOT.AddSkill(new Skill_Punch());
             SKILLSLOT.AddSkill(new Skill_Kick());
-            if (c != null)
-            {
-                // 직업 스킬을 랜덤하게 획득
-                int num = random.Next(c.SKILLSLOT.SKILLS.Length + 1);
-                while (num < c.SKILLSLOT.SKILLS.Length && c.SKILLSLOT.SKILLS[num] != null)
-                {
-                    SKILLSLOT.AddSkill(c.SKILLSLOT.SKILLS[num]);
-                    c.SKILLSLOT.RemoveSkill(num);
-                    num = random.Next(num + 1);
-                    DIFFICULTY += 1;
-                }
-            }
 
             // 아이템 슬롯은 일반적으로 최대 3
             ITEMSLOT = new ItemSlot(this, 3);
             ITEMSLOT.AddItem(new Item_HPPotion1());
+        }
+
+        /// <summary>
+        /// 직업 생성자
+        /// </summary>
+        /// <param name="c">캐릭터의 직업</param>
+        public PC(Class c) : base()
+        {
+            Random random = new Random();
+
+            // 직업 스테이터스 획득
+            MAX_HP += c.HP;
+            MAX_SP += c.SP;
+            MAX_PHYSICSAL += c.PHYSICAL;
+            MAX_MENTAL += c.MENTAL;
+            MAX_INITIATIVE += c.INITIATIVE;
+
+            StatusSetting(true);
+            for (int i = 0; i < 5; i++)
+                DIFFICULTY += status[0, i] / 10;
+
+            // 직업 스킬을 랜덤하게 획득
+            int num = random.Next(c.SKILLSLOT.SKILLS.Length + 1);
+            while (num < c.SKILLSLOT.SKILLS.Length && c.SKILLSLOT.SKILLS[num] != null)
+            {
+                SKILLSLOT.AddSkill(c.SKILLSLOT.SKILLS[num]);
+                c.SKILLSLOT.RemoveSkill(num);
+                num = random.Next(num + 1);
+                DIFFICULTY += 1;
+            }
+        }
+
+        /// <summary>
+        /// 직업 추가 메소드
+        /// </summary>
+        /// <param name="c">추가할 직업</param>
+        public void SetClass(Class c)
+        {
+            Random random = new Random();
+
+            // 직업 스테이터스 획득
+            MAX_HP += c.HP;
+            MAX_SP += c.SP;
+            MAX_PHYSICSAL += c.PHYSICAL;
+            MAX_MENTAL += c.MENTAL;
+            MAX_INITIATIVE += c.INITIATIVE;
+
+            StatusSetting(true);
+            for (int i = 0; i < 5; i++)
+                DIFFICULTY += status[0, i] / 10;
+
+            // 직업 스킬을 랜덤하게 획득
+            int num = random.Next(c.SKILLSLOT.SKILLS.Length + 1);
+            while (num < c.SKILLSLOT.SKILLS.Length && c.SKILLSLOT.SKILLS[num] != null)
+            {
+                SKILLSLOT.AddSkill(c.SKILLSLOT.SKILLS[num]);
+                c.SKILLSLOT.RemoveSkill(num);
+                num = random.Next(num + 1);
+                DIFFICULTY += 1;
+            }
         }
     }
 }
