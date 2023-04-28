97e9c4c16765a4fa32174ceac9679734326e6219
parkrye <parkrye@gmail.com>
2023-04-28 14:21:13 +0900

    feat: Project RPG
    스킬 정리, 몬스터 스킬 추가 및 구분

--- 02_CSharpAlgorithm/PersonalProject/ProjectRPG/Skills/CharacterSkills/Actives/Skill_LuckyAttack.cs
+++ 02_CSharpAlgorithm/PersonalProject/ProjectRPG/Skills/CharacterSkills/Actives/Skill_LuckyAttack.cs
@@ -3,20 +3,20 @@
     /// <summary>
     /// 액티브 스킬을 상속한 상세 스킬 클래스
     /// </summary>
-    internal class Skill_Kick : Skill_Active, IAttackable
+    internal class Skill_LuckyAttack : Skill_Active, IAttackable
     {
         /// <summary>
         /// 생성자
         /// </summary>
         /// <param name="_level">스킬 레벨. 기본 1</param>
         /// <param name="_exp">스킬 경험치. 기본 0</param>
-        public Skill_Kick(int _level = 1, int _exp = 0) : base()
+        public Skill_LuckyAttack(int _level = 1, int _exp = 0) : base()
         {
-            name = "(A)발차기";
+            name = "(A)행운의 일격";
             level = _level;
             exp = _exp;
             value = 1;
-            cost = 0;
+            cost = 5;
         }
 
         public override bool Active(int[,] param1, params int[] param2)
@@ -30,7 +30,7 @@
 
         public bool Attack(IHitable hitable, params int[] param)
         {
-            if(hitable.Hit(param[0] * value * level))
+            if(hitable.Hit(new Random().Next(value * 2 + 1) * param[0] / 2 * level))
             {
                 GetEXP(1);
                 return true;
---
+++ 02_CSharpAlgorithm/PersonalProject/ProjectRPG/Skills/CharacterSkills/Actives/Skill_ThrowDagger.cs
@@ -0,0 +1,41 @@
+﻿namespace ProjectRPG
+{
+    /// <summary>
+    /// 액티브 스킬을 상속한 상세 스킬 클래스
+    /// </summary>
+    internal class Skill_ThrowDagger : Skill_Active, IAttackable
+    {
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        /// <param name="_level">스킬 레벨. 기본 1</param>
+        /// <param name="_exp">스킬 경험치. 기본 0</param>
+        public Skill_ThrowDagger(int _level = 1, int _exp = 0) : base()
+        {
+            name = "(A)단검 투척";
+            level = _level;
+            exp = _exp;
+            value = 2;
+            cost = 5;
+        }
+
+        public override bool Active(int[,] param1, params int[] param2)
+        {
+            if (other != null)
+            {
+                return Attack(other, param1[1,2]);
+            }
+            return false;
+        }
+
+        public bool Attack(IHitable hitable, params int[] param)
+        {
+            if(hitable.Hit(param[0] / 2 * value * level))
+            {
+                GetEXP(1);
+                return true;
+            }
+            return false;
+        }
+    }
+}
