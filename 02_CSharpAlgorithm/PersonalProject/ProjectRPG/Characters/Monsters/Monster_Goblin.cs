b2575f920025335e6bba4d7a8e5f35990a092089
parkrye <parkrye@gmail.com>
2023-04-28 14:20:54 +0900

    feat: Project RPG
    몬스터 추가

---
+++ 02_CSharpAlgorithm/PersonalProject/ProjectRPG/Characters/Monsters/Monster_Goblin.cs
@@ -0,0 +1,27 @@
+﻿namespace ProjectRPG
+{
+    internal class Monster_Goblin : PC
+    {
+        public Monster_Goblin(int index)
+        {
+            name = $"소귀 {index}";
+
+            Random random = new Random();
+
+            status = new int[2, 5];
+            MAX_HP = random.Next(5, 8);
+            MAX_SP = random.Next(5, 8);
+            MAX_PHYSICSAL = random.Next(3, 8);
+            MAX_MENTAL = random.Next(1, 5);
+            MAX_INITIATIVE = random.Next(1, 8);
+            StatusSetting(true);
+
+            SKILLSLOT = new SkillSlot(this, 3);
+            SKILLSLOT.AddSkill(new Skill_Scratch());
+
+            ITEMSLOT = new ItemSlot(this, 3);
+            if(random.Next(10) == 0)
+                ITEMSLOT.AddItem(new Item_WoodenAculpture());
+        }
+    }
+}
---
+++ 02_CSharpAlgorithm/PersonalProject/ProjectRPG/Characters/Monsters/Monster_Orc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProjectRPG
+{
+    internal class Monster_Orc : PC
+    {
+        public Monster_Orc(int index)
+        {
+            name = $"오수귀 {index}";
+
+            Random random = new Random();
+
+            status = new int[2, 5];
+            MAX_HP = random.Next(12, 18);
+            MAX_SP = random.Next(12, 18);
+            MAX_PHYSICSAL = random.Next(5, 10);
+            MAX_MENTAL = random.Next(2, 8);
+            MAX_INITIATIVE = random.Next(2, 10);
+            StatusSetting(true);
+
+            SKILLSLOT = new SkillSlot(this, 3);
+            SKILLSLOT.AddSkill(new Skill_Swing());
+            SKILLSLOT.AddSkill(new Skill_RoughSkin());
+
+            ITEMSLOT = new ItemSlot(this, 3);
+            if(random.Next(10) < 2)
+                ITEMSLOT.AddItem(new Item_WoodenAculpture());
+        }
+    }
+}
