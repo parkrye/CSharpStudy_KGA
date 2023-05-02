b9790d7ca1374884705bd11e04d348914fece09e
parkrye <parkrye@gmail.com>
2023-05-02 21:47:39 +0900

    feat: Study and Homework

---
+++ 02_CSharpAlgorithm/Homework/Homework_10/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace Homework_10
+{
+    internal class Program
+    {
+        const int INF = 99999;
+
+        static void Main(string[] args)
+        {
+            // 사용할 그래프
+            int[,] graph = new int[8, 8]
+                {
+                    { 0, INF, 7, 3, 7, INF, INF, INF },
+                    { INF, 0, INF, INF, INF, INF, INF, INF },
+                    { 7, INF, 0, INF, INF, INF, 4, INF },
+                    { 3, INF, INF, 0, INF, INF, INF, 8 },
+                    { 7, INF, INF, INF, 0, INF, INF, 9 },
+                    { INF, INF, INF, INF, INF, 0, 3, 3 },
+                    { INF, INF, 4, INF, INF, 3, 0, INF },
+                    { INF, INF, INF, 8, 9, 3, INF, 0 }
+                };
+            int[] dist = new int[8];    // 각 노드까지의 거리 배열
+            int[] path = new int[8];    // 각 노드의 이전 노드 배열
+            MyDijkstra(graph, 0, dist, path);
+            PrintMyDijkstra(graph, dist, path);
+        }
+
+        /// <summary>
+        /// 다익스트라 연산 메소드
+        /// </summary>
+        /// <param name="graph">그래프</param>
+        /// <param name="start">시작 노드</param>
+        /// <param name="dist">시작 노드로부터의 거리 배열</param>
+        /// <param name="path">노드의 이전 노드 배열</param>
+        static void MyDijkstra(int[,] graph, int start, int[] dist, int[] path)
+        {
+            bool[] visit = new bool[graph.GetLength(0)];    // 방문 여부 배열
+
+            for (int i = 0; i < graph.GetLength(0); i++)    // 각 노드에 대하여
+            {
+                if(i != start)                              // 시작 노드가 아닌 경우
+                {
+                    if (graph[start, i] < INF)              // 연결되어 있다면
+                    {
+                        dist[i] = graph[start, i];          // 거리는 시작 노드로부터의 거리
+                        path[i] = start;                    // 이전 노드는 시작 노드
+                    }
+                    else                                    // 연결되어 있지 않다면
+                    {
+                        dist[i] = INF;                      // 거리는 무한대
+                        path[i] = -1;                       // 이전 노드는 없음
+                    }
+                }
+                else                                        // 시작 노드인 경우
+                {
+                    dist[i] = 0;                            // 거리는 0
+                    path[i] = -1;                           // 이전 노드는 없음
+                    visit[start] = true;                    // 시작 노드는 방문
+                }
+            }
+
+            for (int count = 0; count < graph.GetLength(1); count++)    // 각 노드 개수만큼 반복
+            {
+                int minIdx = -1;                                        // 가장 거리가 가까운 노드의 번호
+                int minDist = INF;                                      // 가장 가까운 노드와의 거리
+                for(int i = 0; i < graph.GetLength(0); i++)             // 각 노드에 대하여
+                {
+                    if(i != start &&                                    // 시작 노드가 아니고
+                        !visit[i] &&                                    // 방문하지 않았으며
+                        dist[i] < minDist)                              // 거리가 최단 거리 노드보다 작다면
+                    {
+                        minIdx = i;                                     // 해당 노드가 최단 거리 노드이고
+                        minDist = dist[i];                              // 최단 거리느 해당 노드와의 거리
+                    }
+                }
+                if (minIdx == -1)                                       // 최단 노드가 없다면 모든 노드를 방문한 것
+                    break;
+
+                for (int i = 0; i < graph.GetLength(0); i++)            // 각 노드에 대하여
+                {
+                    if (i != start &&                                   // 시작 노드가 아니고
+                        dist[i] > dist[minIdx] + graph[minIdx, i])      // 시작 노드로부터의 거리가 최단 노드를 거쳐서 도달하는 거리보다 크다면
+                    {
+                        dist[i] = dist[minIdx] + graph[minIdx, i];      // 시작 노드로부터의 거리는 최단 노드를 거쳐서 도달하는 거리
+                        path[i] = minIdx;                               // 시작 노드로부터의 노드의 이전 노드는 최단 노드
+                    }
+                }
+
+                visit[minIdx] = true;                                   // 최단 노드는 방문
+            }
+        }
+
+        /// <summary>
+        /// 다익스트라 결과 출력 메소드
+        /// </summary>
+        /// <param name="graph">그래프</param>
+        /// <param name="dist">거리</param>
+        /// <param name="path">이전노드</param>
+        static void PrintMyDijkstra(int[,] graph, int[] dist, int[] path)
+        {
+            int size = graph.GetLength(0);
+            Console.WriteLine("[그래프]");
+            for (int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    Console.Write($"{(graph[i, j] < INF ? graph[i, j] : "-")} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("[최단 거리]");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"{i} : {(dist[i] < INF ? dist[i] : "-")} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("[이전 경로]");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"{i} : {(path[i] != -1 ? path[i] : "-")} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 02_CSharpAlgorithm/Study/12_ShortestPath/FloydWarshall.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_ShortestPath
+{
+    internal class FloydWarshall
+    {
+        /******************************************************
+		 * 플로이드-워셜 알고리즘 (Floyd-Warshall Algorithm)
+		 * 
+		 * 모든 노드에서 출발하여 다른 노드로 가는 각각의 최단 경로를 구함
+		 * 모든 노드를 거쳐가며 최단 거리가 갱신되는 조합이 있을 경우 갱신
+		 ******************************************************/
+
+        const int INF = 99999;
+
+        public static void ShortestPath(in int[,] graph, out int[,] costTable, out int[,] pathTable)
+        {
+            int count = graph.GetLength(0);
+            costTable = new int[count, count];  // 각 정점에서 다른 정점으로의 거리에 대한 배열
+            pathTable = new int[count, count];  // 최단 거리로 각 정점에 도달한 직전 정점
+
+            for (int y = 0; y < count; y++)
+            {
+                for (int x = 0; x < count; x++)
+                {
+                    costTable[y, x] = graph[y, x];
+                    pathTable[y, x] = graph[y, x] < INF ? y : -1;
+                }
+            }
+
+            for (int middle = 0; middle < count; middle++)      // 중간 정점
+            {
+                for (int start = 0; start < count; start++)     // 출발 정점
+                {
+                    if (middle == start)
+                        continue;
+
+                    for (int end = 0; end < count; end++)       // 도착 정점
+                    {
+                        if (middle == end || start == end)
+                            continue;
+
+                        // 출발 => 도착 > 출발 => 중간 => 도착이라면
+                        if (costTable[start, end] > costTable[start, middle] + costTable[middle, end])
+                        {
+                            // 중간을 거치는 경로가 더 짧으니 해당 거리를 저장
+                            costTable[start, end] = costTable[start, middle] + costTable[middle, end];
+                            // 중간 정점을 기록
+                            pathTable[start, end] = middle;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
